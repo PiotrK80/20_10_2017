95e0350c577b1a1464fec4a4d7d2c12b859d5f6e
Your Name <p1m2k3oz@poczta.onet.pl>
2017-10-20 11:24:08 +0200

    pierwszy zrzut

---
+++ program1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> /* biblioteka standardowa input-output */
+
+/* y = f(x) */
+/*	f() */
+
+int main() /* funkcja glowna programu */
+/* int -> typ zwracanej wartosci (int - wartosc calkowita) */
+{
+
+	puts("Pierwszy program"); /* puts - funkcja z biblioteki standardowej */
+				/* wyswietlajaca ciag znakow */
+	return 0;
+}
